93db3c6886779009b4a03c1faf9f669504e0f2a2
yg10323 <1357373713@qq.com>
2022-08-24 16:43:23 +0800

    t

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
 
 export default function App () {
+
+  const testArray: number[] = [123, 345, 13, 34, 1, 234, 23]
+  console.log(testArray.sort((a, b) => b - a));
+
   return (
     <div>App</div>
   )
